05b3ebcd50df210b5bb4c817d091cd3a8d96268a
pythonreus <kgadiselepe11@gmail.com>
2026-01-15 18:48:51 +0200

    added the documentation for the applications routes and how to interact with the api

--- routes/api/applicationRoutes.js
+++ routes/api/applicationRoutes.js
@@ -7,7 +7,7 @@ import { createApplication, getApplicationsPaginated, getApplicationById } from 
 const router = express.Router();
 
 // POST /api/client/application
-router.post("/client/application",verifyAuthToken, createApplication);
+router.post("/",verifyAuthToken, createApplication);
 router.get("/",verifyAuthToken,requireAdmin, getApplicationsPaginated);
 router.get("/:id",verifyAuthToken,requireAdmin, getApplicationById);
 
